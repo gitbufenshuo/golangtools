e2a937cc77341bb965ed6860ad4167baab57920f
王思源 <sywang@firewinggames.com>
2020-11-02 15:48:11 +0800

    edit head

--- learnwebgl/main.go
+++ learnwebgl/main.go
@@ -13,20 +13,31 @@ func main() {
 	thefilename := os.Getenv("FILENAME")
 	fmt.Println(thefilename)
 	segs := strings.Split(thefilename, ".")
-	// frontName := segs[0]
+	frontName := segs[0]
 	suffixName := segs[1]
 	var fileout string
 	var url string
+	var ismodel bool
 	if suffixName == "vert" || suffixName == "frag" {
 		os.Mkdir("shaders", 0755)
 		fileout = path.Join("shaders", thefilename)
 		url = "http://learnwebgl.brown37.net/lib/shaders/" + thefilename
 	} else {
+		ismodel = true
 		os.Mkdir("models", 0755)
 		fileout = path.Join("models", thefilename)
 		url = "http://learnwebgl.brown37.net/lib/models/" + thefilename
 	}
 	//////////////////////////////////////////////////
+	download(url, fileout)
+	if ismodel {
+		url = "http://learnwebgl.brown37.net/lib/models/" + frontName + ".mtl"
+		fileout = frontName + ".mtl"
+		download(url, fileout)
+	}
+}
+
+func download(url, fileout string) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
