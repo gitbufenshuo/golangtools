acb7555d886d488aaca636d6a415490cc51f6862
gitbufenshuo <boaitianxia@163.com>
2022-04-27 09:57:54 +0800

    test ui del

--- httpser/main.go
+++ httpser/main.go
@@ -8,8 +8,14 @@ import (
 )
 
 func main() {
-	e := echo.New()
-	e.Static("/", os.Args[1])
-	fmt.Println(os.Args[2])
-	e.Start(os.Args[2])
+	if len(os.Args) == 1 {
+		e := echo.New()
+		e.Static("/", ".")
+		e.Start("0.0.0.0:9998")
+	} else {
+		e := echo.New()
+		e.Static("/", os.Args[1])
+		fmt.Println(os.Args[2])
+		e.Start(os.Args[2])
+	}
 }
